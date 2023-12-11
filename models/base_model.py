586041df4960d3d3c6ac8f9aa2c5468bf549f38a
koldflames <enoeihoda@gmail.com>
2023-12-10 19:11:56 -0800

    adding the base model class

---
+++ models/base_model.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+"""This is the script for the base model"""
+
+import uuid
+from datetime import datetime
+from models import storage
+
+
+class BaseModel:
+    """Main class from which all other classes will inherit"""
+
+    def __init__(self, *args, **kwargs):
+        """Initializes instance attributes
+
+        Args:
+            - *args: list of arguments
+            - **kwargs: dict of key-values arguments
+        """
+
+        if kwargs:
+            self._handle_kwargs(kwargs)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            storage.new(self)
+
+    def _handle_kwargs(self, kwargs):
+        """Handles keyword arguments and sets instance attributes.
+
+        Args:
+            kwargs (dict): Key/value pairs of attributes.
+        """
+        for key, value in kwargs.items():
+            if key == "created_at" or key == "updated_at":
+                value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+            setattr(self, key, value)
+
+    def __str__(self):
+        """Returns official string representation"""
+
+        return "[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__)
+
+    def save(self):
+        """updates the public instance attribute updated_at"""
+        self.updated_at = datetime.now()
+        storage.save()
+
+    def to_dict(self):
+        """returns a dictionary containing all keys/values of __dict__"""
+        my_dict = self.__dict__.copy()
+        my_dict["__class__"] = type(self).__name__
+        my_dict["created_at"] = my_dict["created_at"].isoformat()
+        my_dict["updated_at"] = my_dict["updated_at"].isoformat()
+        return my_dict
+
